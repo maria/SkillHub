acc4f7b1516edaf1c4bbdbc553e36cbf46de0566
marianitadn <maria.nita.dn@gmail.com>
2014-04-21 15:42:45 +0300

    Add home view, template and url

--- skillhub/hub/views.py
+++ skillhub/hub/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
--- skillhub/urls.py
+++ skillhub/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from hub.views import HomeView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'skillhub.views.home', name='home'),
-    # url(r'^skillhub/', include('skillhub.foo.urls')),
+    url(r'^$', HomeView.as_view(), name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
