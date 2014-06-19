c958bf31c775cf4659358dc991d17f224d0bbd80
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 21:54:48 +0300

    Update import

--- skillhub/hub/views.py
+++ skillhub/hub/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 from hub.connect_github import ConnectGitHub
