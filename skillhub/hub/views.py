1ddbe2c6fba57d5ace95b63a18c98f8a35e20484
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 19:38:38 +0300

    Add GH authorization views and urls

--- skillhub/hub/views.py
+++ skillhub/hub/views.py
@@ -1,5 +1,10 @@
+import urllib2
+
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from hub.connect_github import ConnectGitHub
 
 
 class HomeView(TemplateView):
@@ -7,3 +12,18 @@ class HomeView(TemplateView):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class AuthorizeGitHub(TemplateView):
+
+    def get(self, request):
+        url = ConnectGitHub().authorize_url()
+        return HttpResponseRedirect(url)
+
+
+class ConnectGitHubAccount(TemplateView):
+
+    def get(self, request):
+        code = urllib2.urlparse.parse_qsl(request.url)[-1][-1]
+        account = ConnectGitHub().authorize(code)
+        return HttpResponse({'status': 'OK'})
--- skillhub/urls.py
+++ skillhub/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from hub.views import HomeView
+from hub.views import HomeView, AuthorizeGitHub, ConnectGitHubAccount
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^authorize/', AuthorizeGitHub.as_view(), name='authorize'),
+    url(r'^connect/', ConnectGitHubAccount.as_view(), name='connect'),
     url(r'^admin/', include(admin.site.urls)),
 )
