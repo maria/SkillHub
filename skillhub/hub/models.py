5a258850a24b899121be4943116ef362d1c356ae
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 16:48:53 +0300

    Add models for Account, Skill and Tip

--- skillhub/hub/models.py
+++ skillhub/hub/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Account(models.Model):
+    class Meta:
+        app_label = 'hub'
+
+
+    user = models.OneToOneField(User)
+    github_username = models.CharField(max_length=50)
+    github_url = models.URLField()
+    github_access_token = models.TextField()
+
+
+class Skill(models.Model):
+    class Meta:
+        app_label = 'hub'
+
+    account = models.ManyToManyField(Account)
+    name = models.CharField(max_length=30)
+    level = models.IntegerField()
+
+
+class Tip(models.Model):
+    class Meta:
+        app_label = 'hub'
+
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
