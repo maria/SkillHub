d6ef4b4c36e53cb195b36e12983f83320fdb2d7b
Maria Nita <maria.nita.dn@gmail.com>
2014-06-19 22:53:38 +0300

    Update account and user attributes. Add login method

--- skillhub/hub/models.py
+++ skillhub/hub/models.py
@@ -1,5 +1,6 @@
 from github import Github
 
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -9,21 +10,28 @@ class Account(models.Model):
         app_label = 'hub'
 
     user = models.OneToOneField(User)
-    github_username = models.CharField(max_length=50)
     github_url = models.URLField()
-    github_access_token = models.TextField()
 
     @classmethod
     def save_github_user(cls, token):
         connection = Github(login_or_token=token).get_user()
+        username = connection.url.split('/')[-1]
 
-        user = User(first_name=connection.name, email=connection.email)
+        user = User(first_name=connection.name, email=connection.email,
+                    username=username, password=token)
         user.save()
 
-        account = cls(user=user, github_access_token=token,
-                      github_username=connection.name, github_url=connection.url)
+        account = cls(user=user, github_url=connection.url)
         account.save()
+
         return account
+
+    def login(self):
+        user = authenticate(username=self.user.username,
+                            password=self.user.password)
+        if user is not None and user.is_active:
+            login(request, user)
+
 
 class Skill(models.Model):
     class Meta:
