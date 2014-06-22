c131c6f08c24b9757e5b77cf037a9003bd497cf1
Maria Nita <maria.nita.dn@gmail.com>
2014-06-22 23:42:46 +0300

    Fix lettuce bug. Add step file

--- deploy.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print("Push to GitHub")
-os.system('git push')
-print("Change directory to submodule")
-os.system("cd ~/skillhub/skillhub/")
-print("Push to Heroku")
-os.system("git push heroku master:master")
