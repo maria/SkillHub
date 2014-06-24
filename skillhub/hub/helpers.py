17ba693da2d05d13455c3b58f197fd8d12f32da1
Maria Nita <maria.nita.dn@gmail.com>
2014-06-24 20:35:41 +0300

    Fix typo

--- skillhub/hub/helpers.py
+++ skillhub/hub/helpers.py
@@ -3,7 +3,7 @@ import arrow
 def get_last_month():
     now = arrow.utcnow()
     if now.month != 1:
-        last_mont = now.replace(month=(now.month - 1))
+        last_month = now.replace(month=(now.month - 1))
     else:
         last_month = now.replace(year=(now.year - 1), month=12)
     return last_month.format("YYYY-MM-DD")
