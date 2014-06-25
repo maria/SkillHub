e80ccb2c187cb7fc005357d654c6125f19e72fff
Maria Nita <maria.nita.dn@gmail.com>
2014-06-25 22:36:39 +0300

    Remove dispatch uid

--- skillhub/hub/signals.py
+++ skillhub/hub/signals.py
@@ -8,4 +8,4 @@ def update_badges(sender, instance, **kwargs):
     GiveBadges.get_my_badges(instance)
 
 
-post_save.connect(update_badges, sender=Account, dispatch_uid="update_account_badges")
+post_save.connect(update_badges, sender=Account)
