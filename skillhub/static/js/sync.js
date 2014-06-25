6201eeb5cc1a1ac5423bb51b529da41668510873
Maria Nita <maria.nita.dn@gmail.com>
2014-06-25 14:33:31 +0300

    Update default timeout. Update layout

--- skillhub/static/js/sync.js
+++ skillhub/static/js/sync.js
@@ -1,10 +1,15 @@
 $.ajax({
     url: window.location.origin + "/sync/",
     dataType:"json",
+    timeout: 80000,
     success: function(response) {
         $("#sync-hubot").remove();
     },
     failure: function(response) {
         alert('Something went wrong please refresh.');
-    }
+    },
+    error: function(response){
+        console.log(response);
+        alert('Something went wrong please refresh.');
+    },
 })
